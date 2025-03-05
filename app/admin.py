8125c5627c6cb87d8f937277922757cf57a54e65
endang-ismaya <endang.ismaya@gmail.com>
2025-03-05 23:05:27 +0700

    add general info model and apply to template

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from app.models import GeneralInfo
 
-# Register your models here.
+
+@admin.register(GeneralInfo)
+class GeneralInfoAdmin(admin.ModelAdmin):
+    list_display = ["company_name", "location", "email", "phone"]
+
+    def has_add_permission(self, request):
+        if GeneralInfo.objects.exists():
+            return False  # Disallow adding if a record already exists
+        return True
+
+    def save_model(self, request, obj, form, change):
+        if not change and GeneralInfo.objects.exists():
+            raise ValidationError("Only one record of this type is allowed.")
+        super().save_model(request, obj, form, change)
+
+    # show to disable 'add' permission
+    # def has_add_permission(self, request):
+    #     return False
+
+    # show to disable 'delete' permission
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    # show to disable 'change' permission
+    # def has_change_permission(self, request, obj=None):
+    #     return False
+
+    # show to disable 'view' permission
+    # def has_view_permission(self, request, obj=None):
+    #     return False
--- app/views.py
+++ app/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
 
+from app.models import GeneralInfo
+
+"""
+company_name = models.CharField(max_length=255, default="Company")
+location = models.CharField(max_length=255)
+email = models.EmailField()
+phone = models.CharField(max_length=20)
+open_hours = models.CharField(max_length=100, blank=True, null=True)
+video_url = models.URLField(blank=True, null=True)
+twitter_url = models.URLField(blank=True, null=True)
+facebook_url = models.URLField(blank=True, null=True)
+instagram_url = models.URLField(blank=True, null=True)
+linkedin_url = models.URLField(blank=True, null=True)
+"""
+
 
 def index(request):
-    return render(request, "app/index.html")
+    general_info = GeneralInfo.objects.first()
+    context = {
+        "company_name": general_info.company_name,
+        "location": general_info.location,
+        "email": general_info.email,
+        "phone": general_info.phone,
+        "open_hours": general_info.open_hours,
+        "video_url": general_info.video_url or "#",
+        "twitter_url": general_info.twitter_url or "#",
+        "facebook_url": general_info.facebook_url or "#",
+        "instagram_url": general_info.instagram_url or "#",
+        "linkedin_url": general_info.linkedin_url or "#",
+    }
+    return render(request, "app/index.html", context)
